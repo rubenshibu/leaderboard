af39264529ec0d0360113d4f6efaea2882cf89b1
tonalmathew <tonalmathew1999@gmail.com>
2020-05-27 09:02:07 +0530

    js updated

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var url = "https://spreadsheets.google.com/feeds/cells/1NrVoO6Wqe-3E3kC8hH-MIqy-
   GiphyAJAXCall.addEventListener("load", function (e) {
     var data = e.target.response;
     var response = JSON.parse(data);
-    console.log(response);
+    console.log(response.feed.entry);
+    for(var i=0; i<response.feed.entry.length; i++)
+    {
+        
+      var container = document.querySelector(".table-content");
+      container.innerHTML +=
+      '<div class="col-lg-4  rank">'+
+        response.feed.entry[i].content.$t+
+      '</div><hr>'
+        
+    }
     
   });
