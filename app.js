008de1160d58e8167773d913611bc6f27898c80a
rubenshibu <rubenshibu@gmail.com>
2020-05-27 14:50:52 +0530

    Dark Mode

--- app.js
+++ app.js
@@ -25,3 +25,19 @@ leaderBoard.addEventListener("load", function (e) {
     );
   });
 });
+var options = {
+  bottom: '64px', 
+  right: 'unset', 
+  left: '32px', 
+  time: '0.5s', 
+  mixColor: '#fff', 
+  backgroundColor: '#fff',  
+  buttonColorDark: '#100f2c',  
+  buttonColorLight: '#fff', 
+  saveInCookies: false, 
+  label: '🌓', 
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
