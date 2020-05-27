63978af8e84e2716db12d1df9e271e6bceebac82
tonalmathew <tonalmathew1999@gmail.com>
2020-05-27 10:33:36 +0530

    js updated

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 var url = "https://spreadsheets.google.com/feeds/cells/1NrVoO6Wqe-3E3kC8hH-MIqy-yKrb5n5DcfxsfWYSzD4/1/public/full?alt=json#"
 
   // AJAX Request
-  var GiphyAJAXCall = new XMLHttpRequest();
-  GiphyAJAXCall.open("GET", url);
-  GiphyAJAXCall.send();
+  var leaderBoard = new XMLHttpRequest();
+  leaderBoard.open("GET", url);
+  leaderBoard.send();
 
-  GiphyAJAXCall.addEventListener("load", function (e) {
+  leaderBoard.addEventListener("load", function (e) {
     var data = e.target.response;
     var response = JSON.parse(data);
     console.log(response.feed.entry);
