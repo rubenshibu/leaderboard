77e9443976fa8aab29586be35597b800acdba4a2
shan-shaji <shanshaji224@gmail.com>
2020-05-27 12:27:06 +0530

    Javascript file changed

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
+var url = "https://sheet.best/api/sheets/cb49aab6-348f-4bd3-a698-2fd1a3328929";
 
-var url = "https://spreadsheets.google.com/feeds/cells/1NrVoO6Wqe-3E3kC8hH-MIqy-yKrb5n5DcfxsfWYSzD4/1/public/full?alt=json#"
+// AJAX Request
+var leaderBoard = new XMLHttpRequest();
+leaderBoard.open("GET", url);
+leaderBoard.send();
 
-  // AJAX Request
-  var leaderBoard = new XMLHttpRequest();
-  leaderBoard.open("GET", url);
-  leaderBoard.send();
+leaderBoard.addEventListener("load", function (e) {
+  var data = e.target.response;
+  var response = JSON.parse(data);
+  let resultsData = response;
 
-  leaderBoard.addEventListener("load", function (e) {
-    var data = e.target.response;
-    var response = JSON.parse(data);
-    console.log(response.feed.entry);
-    for(var i=0; i<response.feed.entry.length; i++)
-    {
-        
-      var container = document.querySelector(".table-content");
-      container.innerHTML +=
-      '<div class="col-lg-4 col-md-4 col-sm-4 col-xl-4  rank">'+
-        response.feed.entry[i].content.$t+
-      '</div><hr>'
-        
-    }
-    
+  resultsData.forEach((result) => {
+    console.log(result);
+    $(".card-content").append(
+      `<div>
+        ${result.Rank} ${result.Name} ${result.Points}
+      </div>`
+    );
   });
+});
