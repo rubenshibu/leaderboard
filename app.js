7d59cb72d9985039338f847d5a66755501f4829b
tonalmathew <tonalmathew1999@gmail.com>
2020-05-27 12:15:56 +0530

    js updated

--- app.js
+++ app.js
@@ -10,13 +10,19 @@ var url = "https://spreadsheets.google.com/feeds/cells/1NrVoO6Wqe-3E3kC8hH-MIqy-
     var data = e.target.response;
     var response = JSON.parse(data);
     console.log(response.feed.entry);
-    for(var i=0; i<response.feed.entry.length; i++)
+    for(var i=0; i<response.feed.entry.length; i=i+3)
     {
         
       var container = document.querySelector(".table-content");
       container.innerHTML +=
-      '<div class="col-lg-4 col-md-4 col-sm-4 col-xl-4  rank">'+
+      '<div class="col-lg-2 col-md-2 col-sm-2 col-xl-2  rank">'+
         response.feed.entry[i].content.$t+
+      '</div><hr>'+
+      '<div class="col-lg-7 col-md-7 col-sm-7 col-xl-7  name">'+
+      response.feed.entry[i+1].content.$t+
+      '</div><hr>'+
+      '<div class="col-lg-3 col-md-3 col-sm-3 col-xl-3  point">'+
+      response.feed.entry[i+2].content.$t+
       '</div><hr>'
         
     }
